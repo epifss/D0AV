a2cdc7f35baeca17cea3f0e8b71847cead2fbdfa
epifss <143472124+epifss@users.noreply.github.com>
2025-02-26 15:50:56 -0300

    add readme.md

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        args[args.length - 1]        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
